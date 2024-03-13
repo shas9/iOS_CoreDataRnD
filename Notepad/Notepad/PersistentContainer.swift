0765f849fdfae84318bcd679797f1cd5e723d610
Shahwat Hasnaine <shahwathasnaine@gmail.com>
2024-03-13 15:42:15 +0600

    Create a Persitence Controller class

---
+++ Notepad/Notepad/PersistentContainer.swift
@@ -0,0 +1,36 @@
+//
+//  PersistentContainer.swift
+//  Notepad
+//
+//  Created by Shahwat Hasnaine on 13/3/24.
+//
+
+import CoreData
+
+class PersistentContainer {
+    static let shared = PersistentContainer()
+    let container: NSPersistentContainer
+    
+    private init() {
+        container = NSPersistentContainer(name: "Model")
+        container.loadPersistentStores { storeDescription, error in
+            if let error {
+                fatalError("Could not load Core Data persistence sotres. Error: \(error)")
+            }
+        }
+    }
+    
+    init(inMemory: Bool = false) {
+        container = NSPersistentContainer(name: "Model")
+        
+        if inMemory {
+            container.persistentStoreDescriptions.first?.url = URL(fileURLWithPath: "/dev/null")
+        }
+        
+        container.loadPersistentStores { storeDescription, error in
+            if let error {
+                fatalError("Could not load Core Data persistence sotres. Error: \(error)")
+            }
+        }
+    }
+}
